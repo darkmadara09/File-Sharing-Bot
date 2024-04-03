6d14b7175b7243afd61aa9cac9f8fa28354516a4
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-03 11:33:44 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -7,10 +7,22 @@ from helper_func import get_readable_time
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
-    now = datetime.now()
-    delta = now - bot.uptime
-    time = get_readable_time(delta.seconds)
-    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    storage = psutil.disk_usage('/')
+
+    python_version = platform.python_version()
+
+    reply_text = (
+        f"➪ᴜᴘᴛɪᴍᴇ: {uptime}\n"
+        f"➪ᴄᴘᴜ: {cpu}%\n"
+        f"➪ꜱᴛᴏʀᴀɢᴇ: {size_formatter(storage.total)} [ᴛᴏᴛᴀʟ]\n"
+        f"➪{size_formatter(storage.used)} [ᴜsᴇᴅ]\n"
+        f"➪{size_formatter(storage.free)} [ғʀᴇᴇ]\n"
+        f"➪ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ: {python_version},"
+    )
+
+    await message.reply(reply_text, quote=True)
 
 
 @Bot.on_message(filters.private & filters.incoming)

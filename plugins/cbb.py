5ec15f8595b72ddde355da118442cc27c499673c
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-10-05 11:44:25 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/demon_city_x'>Click here</a>\n○ Channel : @shadows_network01\n○ Support Group : @ix_bot_support_gc</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Crunchyroll_Anime_in_Hindi_Dub_l'>Click here</a>\n○ Channel : @Crunchyroll_Anime_in_Hindi_Dub_l\n○ Support Group : @Rg_Anime_in_Hindi_Group_KDrama</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [

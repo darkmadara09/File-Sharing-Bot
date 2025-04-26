de84b28dd805e3634e24552b859b8dc7277adb16
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2025-04-26 15:12:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,9 +29,9 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Avon:Avon@avon.fstai.mong
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002193678374","-1002165537292"))
+FORCE_SUB_CHANNEL = "-1002193678374","-1002165537292"
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "3"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
